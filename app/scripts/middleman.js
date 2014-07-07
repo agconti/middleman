d4c8aa09bc2f67c327a5a3600f0e3e19e9f1e397
awc-fueled <conti@fueled.com>
2014-07-07 12:26:33 -0400

    feat(middleman): init scaffolding

---
+++ app/scripts/middleman.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('middleman', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ngRoute'])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'partials/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  })
+;
